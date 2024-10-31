a7257813b173f1eac608f220671bd53bd32fbab7
Josip Bošnjak <josip-bosnjak30{gmail.com>
2024-10-31 14:55:59 +0100

    update6

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/website/', // Specify your repo name here
+  plugins: [react(), ghPages()], // Ensure ghPages is being called as a function
+  base: '/website/', // Make sure this matches your GitHub Pages repo name
 });
