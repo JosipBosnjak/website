f4e06bf1299c7cf6457e4d8d82f5be3f0a9515f0
Josip Bošnjak <josip-bosnjak30{gmail.com>
2024-12-23 22:24:42 +0100

    last

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Menu, X } from 'lucide-react';
-import logo from '../assets/tools.svg';
+import logo from '../assets/logo.svg';
 import { navItems } from '../constants';
 import { useState } from 'react';
 
@@ -15,15 +15,16 @@ const Navbar = () => {
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
             <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
-            <span className="text-xl tracking-tight">VirtualR</span>
+            <span className="text-xl tracking-tight">Promilćek</span>
           </div>
-          <ul className="hidden lg:flex ml-14 space-x-12">
+          {/*<ul className="hidden lg:flex ml-14 space-x-12">
             {navItems.map((item, index) => (
               <li key={index}>
                 <a href="{item.href}">{item.label}</a>
               </li>
             ))}
           </ul>
+          */}
           <div className="hidden lg:flex justify-center space-x-12 items-center">
             <a href="#" className="py-2 px-3 border rounded.md">
               Sign in
