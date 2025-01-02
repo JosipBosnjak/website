6da13d0abe87eb2834950c075b85c66092ae2238
Josip Bošnjak <josip-bosnjak30{gmail.com>
2025-01-02 18:40:00 +0100

    ComingSoon Update 4

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,11 +26,11 @@ const Navbar = () => {
           </ul>
           */}
           <div className="hidden lg:flex justify-center space-x-12 items-center">
-            <a href="#" className="py-2 px-3 rounded.md">
+            <a href="https://promilcek.com/" className="py-2 px-3 rounded.md">
               Coming soon
             </a>
             <a
-              href="#"
+              href="https://promilcek.com/"
               className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
             >
               Coming soon
@@ -52,14 +52,17 @@ const Navbar = () => {
               ))}
             </ul>
             <div className="flex space-x-6">
-              <a href="#" className="py-2 px-3 border rounded-md">
-                Sing In
+              <a
+                href="https://promilcek.com/"
+                className="py-2 px-3 border rounded-md"
+              >
+                Coming soon
               </a>
               <a
-                href="#"
+                href="https://promilcek.com/"
                 className="py-2 px-3 rounded-md bg-gradient-to-t from-orange-500 to-orange-800"
               >
-                Create an account
+                Coming soon
               </a>
             </div>
           </div>
